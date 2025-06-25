e866e54699e3cada6db89f11a7dab23dd2ac7b0d
Jo <joachim.dev@pm.me>
2025-06-25 21:44:39 +0200

    change colorscheme to catppuccin mocha

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -7,7 +7,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'catppuccin-mocha', 'catppuccin-macchiato', or 'catppuccin-latte'.
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
