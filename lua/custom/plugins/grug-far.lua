e5a764f0a75ee3077a691cc091a8bda4eed6067a
Jo <joachim.dev@pm.me>
2025-06-25 13:28:29 +0200

    add grug-far search and replace

---
+++ lua/custom/plugins/grug-far.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'MagicDuck/grug-far.nvim',
+    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+    -- additional lazy config to defer loading is not really needed...
+    config = function()
+      -- optional setup call to override plugin options
+      -- alternatively you can set options with vim.g.grug_far = { ... }
+      require('grug-far').setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+      }
+
+      vim.keymap.set('n', '<leader>sR', require('grug-far').open, { desc = '[S]earch and [R]eplace' })
+    end,
+  },
+}
