e580f6f47917f5cb10d47c27843728d5372d6ffd
Jo <joachim.dev@pm.me>
2025-06-26 14:21:20 +0200

    update neorg config

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -3,6 +3,33 @@ return {
     'nvim-neorg/neorg',
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = '*', -- Pin Neorg to the latest stable release
-    config = true,
+    dependencies = {
+      {
+        'folke/zen-mode.nvim',
+        opts = {},
+      },
+    },
+    opts = {
+      load = {
+        ['core.defaults'] = {},
+        ['core.presenter'] = {
+          config = {
+            zen_mode = 'zen-mode',
+          },
+        },
+        ['core.concealer'] = {
+          config = { -- We added a `config` table!
+            icon_preset = 'varied', -- And we set our option here.
+          },
+        },
+        ['core.dirman'] = {
+          config = {
+            workspaces = {
+              notes = '~/notes',
+            },
+          },
+        },
+      },
+    },
   },
 }
