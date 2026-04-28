533c3806e6f098efe16d211a5223f6ba55f85bd0
Jo <joachim.dev@pm.me>
2026-04-28 11:16:37 +0200

    change treesitter

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -1,35 +1,38 @@
-return {
-  {
-    'nvim-neorg/neorg',
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = '*', -- Pin Neorg to the latest stable release
-    dependencies = {
-      {
-        'folke/zen-mode.nvim',
-        opts = {},
-      },
-    },
-    opts = {
-      load = {
-        ['core.defaults'] = {},
-        ['core.presenter'] = {
-          config = {
-            zen_mode = 'zen-mode',
-          },
-        },
-        ['core.concealer'] = {
-          config = { -- We added a `config` table!
-            icon_preset = 'varied', -- And we set our option here.
-          },
-        },
-        ['core.dirman'] = {
-          config = {
-            workspaces = {
-              notes = '~/notes',
-            },
-          },
-        },
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   {
+--     'nvim-neorg/neorg',
+--     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+--     version = '*', -- Pin Neorg to the latest stable release
+--     dependencies = {
+--       {
+--         'folke/zen-mode.nvim',
+--         opts = {},
+--       },
+--       'nvim-neorg/tree-sitter-norg',
+--       'nvim-neorg/tree-sitter-norg-meta',
+--     },
+--     opts = {
+--       load = {
+--         ['core.defaults'] = {},
+--         ['core.presenter'] = {
+--           config = {
+--             zen_mode = 'zen-mode',
+--           },
+--         },
+--         ['core.concealer'] = {
+--           config = { -- We added a `config` table!
+--             icon_preset = 'varied', -- And we set our option here.
+--           },
+--         },
+--         ['core.dirman'] = {
+--           config = {
+--             workspaces = {
+--               notes = '~/notes',
+--             },
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
